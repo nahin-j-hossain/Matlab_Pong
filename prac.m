99682262a7c11c7a8de2d03018cb1cdbcf39ebda
nahin-j-hossain <65483494+nahin-j-hossain@users.noreply.github.com>
2020-05-23 05:39:16 +0000

    Add files via upload
    Open in Matlab.

---
+++ prac.m
@@ -0,0 +1,64 @@
+clear; clc; close all;
+
+%-----------------------------Setup--------------------------------%
+pongFigure = figure('color',[.6 .6 .8], ...  %figure
+    'KeyPressFcn',@keyboardFunction,...
+    'units', 'normal','position',[.1 .1 .8 .8]);
+
+pongAxes = axes('color','black',...          %axes
+    'XLim', [0 100], 'YLim', [-5 100],'XTickLabels',[],...
+    'YTickLabels',[],'position', [.05 .05 .9 .9]);
+
+pongBallVel = [1 , 1];                       %ball
+pongBallPos = [20,50];
+pongBall = line(pongBallPos(1),pongBallPos(2), ...
+    'marker', '.', 'markersize',50,'color', [.8 .6 .6]);
+
+global pongBlockCenter
+pongBlockCenter = 45;
+pongBlock = line([pongBlockCenter - 5, pongBlockCenter + 5 ], [0 0],...   %block
+    'linewidth',4,'color',[.6 .8 .6]);
+
+
+
+%--------------------------Loop----------------------%
+tic;
+while toc<15
+    
+    if pongBallPos(1)<0 || pongBallPos(1) >100  %bounce check
+        pongBallVel(1) = -pongBallVel(1);
+    end
+    if pongBallPos(2) <0 || pongBallPos(2) >100
+        pongBallVel(2) = -pongBallVel(2);
+    end
+    if pongBallPos(2) <0 
+        if abs(pongBallPos(1) -pongBlockCenter) <5
+            pongBallPos(2) = -pongBallVel(2);
+            fprintf ('W ')
+        else
+            fprintf ('GAME OVER')
+            close all; return;
+        end
+    end
+    
+    pongBallPos = pongBallPos + pongBallVel;      %update ball
+    set (pongBall,'Xdata', pongBallPos(1),'YData', pongBallPos(2));
+    
+    set(pongBlock, 'XData',[pongBlockCenter - 5,... %update block
+                            pongBlockCenter + 5])
+                        pause (.02);
+                      
+end
+
+
+%----------------Functions--------------%
+function keyboardFunction (figure, event)
+global pongBlockCenter
+switch event.Key
+    case 'leftarrow'
+    pongBlockCenter = pongBlockCenter -2;
+    case 'rightarrow'
+        pongBlockCenter = pongBlockCenter +2;
+end
+end
+
